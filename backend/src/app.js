f892c9093ade2107f6c625047cb96ef21d7f565e
Your Name <alfasean22@gmail.com>
2024-06-13 22:42:25 +0800

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const policyRoutes = require('./routes/policyRoutes');
 
 const app = express();
+
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use('/api/policies', policyRoutes);
 

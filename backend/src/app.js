7042e50565b6b7b8c93fb9e1616dd9b6bef3ed97
Your Name <alfasean22@gmail.com>
2024-06-14 00:59:06 +0800

    update backend for deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const policyRoutes = require('./routes/policyRoutes');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+app.use((req, res, next) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      res.status(500).send('Error connecting to database');
+    } else {
+      req.pool = connection;
+      next();
+    }
+  });
+});
+
 app.use('/api/policies', policyRoutes);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
